2bd56fa2c1afe0d8d8022e7dbdcb0ecffb4f617d
Влад Наумович <naumovichvlad.2001@gmail.com>
2021-11-05 20:30:38 +0300

    v0.02

---
+++ TaskFour/SlaeCalculatorLib/Calculators/GaussMethodBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SlaeCalculatorLib.Calculators
+{
+    public abstract class GaussMethodBase : IGaussMethod
+    {
+        public abstract double[] Compute(double[,] a, double[] b);
+
+        protected void ExecuteForwardPhaseIteration(double[,] matrix, double[] vector, int iteration)
+        {
+            var n = vector.Length;
+            for (int i = iteration + 1; i < n; i++)
+            {
+                for (int j = iteration + 1; j < n; j++)
+                {
+                    matrix[i, j] = matrix[i, j] - matrix[iteration, j] * (matrix[i, iteration] / matrix[iteration, iteration]);
+                }
+                vector[i] = vector[i] - vector[iteration] * matrix[i, iteration] / matrix[iteration, iteration];
+            }
+        }
+
+        protected void ExecuteBackPhaseIteration(double[,] matrix, double[] vector, double[] answers, int iteration)
+        {
+            double sum = 0;
+            var n = vector.Length;
+            for (int j = iteration + 1; j < n; j++)
+                sum += matrix[iteration, j] * answers[j];
+            answers[iteration] = (vector[iteration] - sum) / matrix[iteration, iteration];
+        }
+
+        protected int FindMainElement(double[,] matrix, int iteration)
+        {
+            var max = Math.Abs(matrix[iteration, iteration]);
+            var maxIndex = iteration;
+            for (int i = iteration + 1; i < matrix.GetLength(0); i++)
+            {
+                if (Math.Abs(matrix[i, iteration]) > max)
+                {
+                    max = Math.Abs(matrix[i, iteration]);
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        protected void SwapRows(double[,] matrix, double[] vector, int mainRowIndex, int iteration)
+        {
+            for (int j = 0; j < vector.Length; j++)
+            {
+                var temp = matrix[iteration, j];
+                matrix[iteration, j] = matrix[mainRowIndex, j];
+                matrix[mainRowIndex, j] = temp;
+            }
+            var tmp = vector[iteration];
+            vector[iteration] = vector[mainRowIndex];
+            vector[mainRowIndex] = tmp;
+        }
+    }
+}
--- TaskFour/SlaeCalculatorLib/Calculators/GaussMethodLinear.cs
+++ TaskFour/SlaeCalculatorLib/Calculators/GaussMethodLinear.cs
@@ -1,58 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SlaeCalculatorLib.Calculators
 {
-    public class GaussMethodLinear
+    public class GaussMethodLinear : GaussMethodBase
     {
-        public double[] Compute(double[,] a, double[] b)
+        public override double[] Compute(double[,] a, double[] b)
         {
             int n = b.Length;
             double[] x = new double[n];
 
-            for (int k = 0; k < n - 1; k++)
+            for (var i = 0; i < n - 1; i++)
             {
-                var max = Math.Abs(a[k, k]);
-                var maxIndex = k;
-                for (int i = k + 1; i < n; i++)
-                {
-                    if (Math.Abs(a[i, k]) > max)
-                    {
-                        max = Math.Abs(a[i, k]);
-                        maxIndex = i;
-                    }
-                }
-
-                for (int j = 0; j < n; j++)
-                {
-                    var temp = a[k, j];
-                    a[k, j] = a[maxIndex, j];
-                    a[maxIndex, j] = temp;
-                }
-                var tmp = b[k];
-                b[k] = b[maxIndex];
-                b[maxIndex] = tmp;
-
-                for (int i = k + 1; i < n; i++)
-                {
-                    for (int j = k + 1; j < n; j++)
-                    {
-                        a[i, j] = a[i, j] - a[k, j] * (a[i, k] / a[k, k]);
-                    }
-                    b[i] = b[i] - b[k] * a[i, k] / a[k, k];
-                }
+                var mainRowIndex = FindMainElement(a, i);
+                SwapRows(a, b, mainRowIndex, i);
+                ExecuteForwardPhaseIteration(a, b, i);
             }
 
-            for (int k = n - 1; k >= 0; k--)
-            {
-                double sum = 0;
-                for (int j = k + 1; j < n; j++)
-                    sum += a[k, j] * x[j];
-                x[k] = (b[k] - sum) / a[k, k];
-            }
+            for (var i = n - 1; i >= 0; i--)
+                ExecuteBackPhaseIteration(a, b, x, i);
+
             return x;
         }
     }
